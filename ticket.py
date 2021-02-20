f077ab55e0c2d7624f5957f78477d207da59b1b5
Mannymunjuri <emmanuel.munjuri@gmail.com>
2021-02-21 00:44:54 +0300

    initial commit

--- ticket.py
+++ ticket.py
@@ -1,46 +1,11 @@
-age=int(input("Enter age"))
+ticket = int(200)
+amount = int(input("Enter Amount"))
+balance =int(amount - ticket)
+print(("Balance:") + str(balance))
+age=int(input("Enter Age"))
 if age>18:
-    print("over 18")
+    print("Over 18. Welcome to Club House.")
+    print(("Balance:") +  str(balance))
 else:
-    print("18 or Under")   
-ticket=int(200)
-amount=int(input("Enter amount"))
-balance=int(amount - ticket)
-print(balance)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
+        print("18 or under.NOT ALLOWED")
+        print(("Balance:")  + str(amount))
