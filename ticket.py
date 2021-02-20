d9b83558174b4539740f6c0c28d9e45755beaeb6
Mannymunjuri <emmanuel.munjuri@gmail.com>
2021-02-20 15:43:08 +0300

    initial commit

---
+++ ticket.py
@@ -0,0 +1,46 @@
+age=int(input("Enter age"))
+if age>18:
+    print("over 18")
+else:
+    print("18 or Under")   
+ticket=int(200)
+amount=int(input("Enter amount"))
+balance=int(amount - ticket)
+print(balance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
